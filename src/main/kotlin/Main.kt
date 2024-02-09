55e7a6ae8765dc2a28b25968d0dbb62747ebd2ea
Karenmariaphilip <karenmariaphilip@gmail.com>
2024-02-09 06:29:09 +0300

    added file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+fun main(){var fullName = "Karen Philip"
+
+ var age = 20
+
+ var contactNumber =797985806
+
+ var kg = 50.3F
+
+ var kenyan = true
+println(fullName)
+ println(age)
+ println(contactNumber)
+ println(kg)
+ println(kenyan)
+
+
+}
+
+
+
+
+
+
+
+
+
